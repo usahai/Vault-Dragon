741f15123af26f98268bffca1efc4a8b353639a1
Udbhav Sahai <usahai@users.noreply.github.com>
2021-07-18 00:14:15 +0800

    Update to a default route
    Will show a text if the end point is hit in the browser

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 router.use("/", require("./v1.0"));
 
+router.get("/", async (req, res, next) => {
+	try {
+		res.status(200).send("Node server is up and running");
+	} catch (error) {
+		res.send(400).send("Failed to set up node server");
+	}
+});
+
 module.exports = router;
