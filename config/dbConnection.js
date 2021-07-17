a600c99e12608d10bc7c8307834ee7b53bcc85aa
Udbhav Sahai <usahai@users.noreply.github.com>
2021-07-17 22:58:19 +0800

    Removal of console.log statements

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,5 @@
 const dbConnect = require("mysql2");
 
-console.log("[DBC] env: ", process.env.NODE_ENV);
-console.log("[DBC] host: ", process.env.DB_HOST);
-console.log("[DBC] user: ", process.env.DB_USER);
-console.log("[DBC] pass: ", process.env.DB_PASS);
-
 const connection = dbConnect.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
