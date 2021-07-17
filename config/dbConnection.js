7096ea5c558d223b2868d597772cb132ed988dee
Udbhav Sahai <usahai@users.noreply.github.com>
2021-07-18 00:06:37 +0800

    Update dbConnection.js
    Updating to include port number

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,7 @@ const dbConnect = require("mysql2");
 
 const connection = dbConnect.createConnection({
 	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_DATABASE
